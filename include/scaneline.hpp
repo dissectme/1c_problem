204fda5991953a4df4c1a5dc11ffe2e23b914789
Andrew Orazov <orazov.ae@phystech.edu>
2021-04-14 20:11:52 +0300

    intermediate

---
+++ include/scaneline.hpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+class Event {
+ public:
+ private:
+};
+
+class Field {
+ public:
+  std::vector<size_t> ScanArea(int x_begin, int y_begin, int dx, int dy) {
+    std::vector<size_t> x_sums(dx);
+    for (int i = x_begin; i < x_begin + dx; i++) {
+      //  _||_||_
+      //  _||_||_
+      //   || ||
+      size_t x_sum = 0;
+      for (int j = y_begin; j < y_begin + dy; j++) {
+        if (matrix_[i][j] == 1) {
+          x_sum++;
+        }
+      }
+
+      x_sums[i] = x_sum;
+    }
+
+    return x_sums;
+  }
+
+  std::vector<size_t> GetMaxConcentrationXes(const std::vector<size_t>& sums) {
+    size_t max = 0;
+    std::vector<size_t> xes;
+    for (size_t i = 0; i < sums.size(); i++) {
+      if (sums[i] > max) {
+        max = sums[i];
+        xes.clear();
+        xes.push_back(i);
+      } else if (sums[i] == max) {
+        xes.push_back(i);
+      }
+    }
+    return xes;
+  }
+
+  std::vector<size_t> CondenseXes(const std::vector<size_t>& max_xes) {
+    size_t begin1 = 0, begin2 = 0, end1 = 0, end2 = 0;
+    for (size_t i = 0; i < max_xes.size(); i++) {
+      if (max_xes[i + 1] != max_xes[i] + 1) {
+        begin2 = max_xes[i + 1];
+        end1 = max_xes[i];
+      }
+    }
+    return { begin1, end1, begin2, begin2 + () }
+  }
+
+ private:
+  using pixel = unsigned;
+  std::vector<std::vector<pixel>> matrix_;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
+#include <decoder.hpp>
+#include <fstream>
 #include <iostream>
+#include <string>
 
-int main(int argc, char** argv) {
+using namespace solution;
 
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    std::cout << "Criss-cross" << std::endl;
+    std::cout << "Usage: " << argv[0] << " .PNG " << std::endl;
 
+    return 0;
+  }
+
+  std::string filename(argv[1]);
+  std::ifstream file(filename);
+
+  Decoder decoder(file);
+  CodeTree code_tree = std::move(decoder).gen_code_tree();
+
+  for (;;) {
+    std::string code;
+    std::cout << "Specify Code (Ctrl-D to exit) > ";
+    if (!(std::cin >> code)) {
+      std::cout << std::endl;
+      break;
+    }
+
+    auto result = code_tree.check_code(code);
+    if (!result) {
+      std::cout << "Code Not Found" << std::endl;
+    } else {
+      std::cout << "Found code at position: " << *result + 1 << std::endl;
+    }
+  }
 }
